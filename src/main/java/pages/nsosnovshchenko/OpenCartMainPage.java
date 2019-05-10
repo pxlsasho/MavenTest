50c1b7269eb601c84f7237965e2c5b9570d25e8e
Nataliia Sosnovshchenko <sonata97@ukr.net>
2019-05-10 16:27:53 +0300

    "HW SosnovshchenkoNS"

---
+++ src/main/java/pages/nsosnovshchenko/OpenCartMainPage.java
@@ -0,0 +1,40 @@
+package pages.nsosnovshchenko;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class OpenCartMainPage {
+    //написать метод который будет переходить на страничку логин в опен карт
+
+    private static final String BASE_URL = "https://demo.opencart.com/";
+    private static final String MY_ACCOUNT_LINK_TEST = "My Account";
+    private static final String LOGIN_LINK_TEST = "Login";
+
+    private WebDriver driver;
+
+    @FindBy(linkText = MY_ACCOUNT_LINK_TEST)
+    private WebElement myAccountLink;
+    @FindBy(linkText = LOGIN_LINK_TEST)
+    private WebElement myLoginLink;
+
+    public OpenCartMainPage(WebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    public void open()
+    {
+        driver.get(BASE_URL);
+    }
+
+    public void goToMyAccount()
+    {
+        myAccountLink.click();
+    }
+
+    public void goToMyLoginPage()
+    {
+        myLoginLink.click();
+    }
+}
---
+++ src/test/java/nsosnovshchenko/OpenCartTest.java
@@ -0,0 +1,28 @@
+package nsosnovshchenko;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import pages.nsosnovshchenko.OpenCartMainPage;
+
+
+public class OpenCartTest {
+
+    @Test
+    public void testOpenCart() {
+        //init chrome driver
+        System.setProperty("webdriver.chrome.driver", "G:\\MyF\\SoftServe\\drivers\\chromedriver.exe");
+        WebDriver ChromeDriver = new ChromeDriver();
+
+        OpenCartMainPage OpenCartMainPage = PageFactory.initElements(ChromeDriver, OpenCartMainPage.class);
+        new OpenCartMainPage(ChromeDriver);
+
+        OpenCartMainPage.open();
+        OpenCartMainPage.goToMyAccount();
+        OpenCartMainPage.goToMyLoginPage();
+
+
+    }
+
+}
