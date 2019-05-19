d29b9595eac939f536119e62dffdd9a9bfa73b37
Nataliia Sosnovshchenko <sonata97@ukr.net>
2019-05-19 22:52:31 +0300

    added new tests

--- src/main/java/nsosnovshchenko/OpenCartMainPage.java
+++ src/main/java/nsosnovshchenko/OpenCartMainPage.java
@@ -17,24 +17,35 @@ public class OpenCartMainPage {
     private WebElement myAccountLink;
     @FindBy(linkText = LOGIN_LINK_TEST)
     private WebElement myLoginLink;
+    @FindBy(id = "input-email")
+    private WebElement emailLog;
+    @FindBy(id = "input-password")
+    private WebElement passwordLog;
+    @FindBy(xpath = "/html/body/div[2]/div/div/div/div[2]/div/form/input")
+    private WebElement loginButton;
 
     public OpenCartMainPage(WebDriver driver)
     {
         this.driver = driver;
     }
-
     public void open()
     {
         driver.get(BASE_URL);
     }
-
     public void goToMyAccount()
     {
         myAccountLink.click();
     }
-
     public void goToMyLoginPage()
     {
         myLoginLink.click();
+    }
+    public void userLogin(){
+        emailLog.clear();
+        emailLog.sendKeys("test@ukr.net");
+        passwordLog.clear();
+        passwordLog.sendKeys("Password12#");
+        loginButton.click();
+
     }
 }
--- src/test/java/nsosnovshchenko/OpenCartTest.java
+++ src/test/java/nsosnovshchenko/OpenCartTest.java
@@ -9,6 +9,21 @@ import org.openqa.selenium.support.PageFactory;
 public class OpenCartTest {
 
     @Test
+    public void registerNewUser() {
+        //init chrome driver
+        System.setProperty("webdriver.chrome.driver", "G:\\MyF\\SoftServe\\drivers\\chromedriver.exe");
+        WebDriver ChromeDriver = new ChromeDriver();
+
+        RegisterUser RegisterUser = PageFactory.initElements(ChromeDriver, RegisterUser.class);
+        new RegisterUser(ChromeDriver);
+
+        RegisterUser.open();
+        RegisterUser.goToMyAccount();
+        RegisterUser.goToRegistrationPage();
+        RegisterUser.registerNewUser();
+    }
+
+    @Test
     public void testOpenCart() {
         //init chrome driver
         System.setProperty("webdriver.chrome.driver", "G:\\MyF\\SoftServe\\drivers\\chromedriver.exe");
@@ -20,8 +35,21 @@ public class OpenCartTest {
         OpenCartMainPage.open();
         OpenCartMainPage.goToMyAccount();
         OpenCartMainPage.goToMyLoginPage();
+        OpenCartMainPage.userLogin();
+    }
 
+    @Test
+    public void searchAndAddToCart(){
+        System.setProperty("webdriver.chrome.driver", "G:\\MyF\\SoftServe\\drivers\\chromedriver.exe");
+        WebDriver ChromeDriver = new ChromeDriver();
 
+        CartPage CartPage = PageFactory.initElements(ChromeDriver, CartPage.class);
+        new CartPage(ChromeDriver);
+
+        CartPage.open();
+        //CartPage.searchAndAddToCart();
+
+        CartPage.searchAndAddToCartBetterTry();
     }
 
 }
