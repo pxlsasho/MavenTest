85a6bbff8ee4cf1c3f8dd6e79db848f4604b4cae
Elfreality <44545596+Elfreality@users.noreply.github.com>
2019-05-28 13:49:03 +0300

    Add files via upload

---
+++ src/main/java/Efik/MainPage.java
@@ -0,0 +1,29 @@
+package Efik;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MainPage {
+        private static final String Base_url = "https://demo.opencart.com/";
+
+        private WebDriver driver;
+        @FindBy(linkText = "My Account")
+        private WebElement myAccountLink;
+
+
+        public MainPage(WebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void OpenWebsite()
+        {
+            driver.get(Base_url);
+        }
+        public void FindAccountLink()
+        {
+            myAccountLink.click();
+        }
+
+    }
